98f986e2d013a688d3c7da277d718c5a14acc042
BisiDavi <oludavidconnect@gmail.com>
2022-11-07 17:10:44 +0100

    buy product function added

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::collections::UnorderedMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, AccountId, near_bindgen, PanicOnDefault};
+use near_sdk::{env, AccountId, near_bindgen, PanicOnDefault, Promise};
 use near_sdk::serde::{Serialize, Deserialize};
 
 #[near_bindgen]
@@ -27,7 +27,7 @@ impl Product {
         Self { id: payload.id, name:payload.name, description: payload.description, image: payload.image, location: payload.location, price: payload.price, owner:env::signer_account_id(), sold: 0 }
     }
 
-    pub fn increment_sold_amont(&mut self){
+    pub fn increment_sold_amount(&mut self){
         self.sold = self.sold + 1;
     }
 }
@@ -65,5 +65,22 @@ impl Marketplace{
     pub fn get_products(&self) -> Vec<Product>{
         return self.listed_products.values_as_vector().to_vec();
     }
+
+    #[payable]
+    pub fn buy_product(&mut self, product_id: &String){
+        match self.listed_products.get(product_id){
+            Some(ref mut product) => {
+                let price = product.price.parse().unwrap();
+                assert_eq!(env::attached_deposit(),price , "attached deposit should be equal to the product's");
+                let owner = &&product.owner.as_str();
+                Promise::new (owner.parse().unwrap()).transfer(price);
+                product.increment_sold_amount();
+                self.listed_products.insert(&product.id, &product);
+            }, _ => {
+                env::panic_str("Product not found");
+            }
+        }
+    }
 }
 
+   
